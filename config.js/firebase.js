3fff05d5e6e23efb466d3ef33dbd7cf5e2f1ea20
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-09-05 23:13:12 -0400

    inicio de api nada con firebase y vuetify

---
+++ config.js/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDVll5dTG0kmGmkrsLHxZ3xMNhKsMoNZfE",
+  authDomain: "api-nasa-b4d23.firebaseapp.com",
+  databaseURL: "https://api-nasa-b4d23.firebaseio.com",
+  projectId: "api-nasa-b4d23",
+  storageBucket: "api-nasa-b4d23.appspot.com",
+  messagingSenderId: "310054406718",
+  appId: "1:310054406718:web:47e0b3398a4495e7c91f6f"
+}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+import firebaseConfig from '../config/firebase.js'
+
+firebase.initializeApp(firebaseConfig);
+
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app =
+      new Vue({
+      router,
+      store,
+      render: h => h(App)
+      }).$mount('#app')
+  }
+})
