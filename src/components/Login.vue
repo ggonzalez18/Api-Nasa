7977f442d1d184528aa2bf4fccebfb79d6aadb03
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-09-05 23:28:14 -0400

    componente login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title><h1 class="mx-auto">Login</h1></v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="userName" prepend-icon="mdi-account-circle"/>
+          <v-text-field
+          @click:append="showPassword = !showPassword"
+          label="password"
+          :type="showPassword ? 'text' : 'password'"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"/>
+        </v-form>
+      </v-card-text>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-
+    data: () => ({
+    showPassword: false
+  }),
 }
 </script>
 
